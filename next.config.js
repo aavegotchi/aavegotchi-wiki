f9e85f50bf21a81952ca396060b7d166d7162d02
Daniel M <dmathieuva@gmail.com>
2020-03-02 20:08:06 +0530

    Fixed markdown loader issue

--- next.config.js
+++ next.config.js
@@ -3,27 +3,12 @@ const glob = require('glob')
 module.exports = {
 
 
-  target: 'serverless',
-  cssLoaderOptions: {
-    url: false
-  },
-  webpack: (config, { isServer }) => {
-
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
-
-    if (typeof require !== 'undefined') {
-      require.extensions['.less'] = () => { }
-      require.extensions['.css'] = file => { }
-    }
-
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader"
+    });
     return config;
-  }
-
-
+  },
 
 };
