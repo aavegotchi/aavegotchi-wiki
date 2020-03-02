d1c7f04fc8f02196fce0a03769c3a691a53cbaf2
Daniel M <dmathieuva@gmail.com>
2020-03-02 19:26:28 +0530

    Added index.html

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8273a5da854b47516653";
+/******/ 	var hotCurrentHash = "2859291dbc850bb15bd0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
