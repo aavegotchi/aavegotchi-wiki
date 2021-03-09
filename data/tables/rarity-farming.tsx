260ab56298f7278603fac6c65001c5b5fa80f8fa
unintelligent-nerd <unintelligent.nerd@gmail.com>
2021-03-09 14:32:42 +0800

    Updated table in Rarity Farming page

--- data/tables/rarity-farming.tsx
+++ data/tables/rarity-farming.tsx
@@ -20,11 +20,13 @@ export const tables
                 data: [
                     ["Energy", "Common", "61", "Calm", "62", "40 (60% possess)"],
                     ["Aggressiveness", "Uncommon", "78", "Combative", "79", "80 (20% possess)"],
-                    ["Spookiness", "Common", "27", "Neutral", "73", "45 (55% possess)"],
+                    ["Spookiness", "Common", "27", "Scary", "73", "45 (55% possess)"],
                     ["Brain Size", "Mythical", "99", "Galaxy", "100", "97 (3% possess)"],
-                    ["Wizard Hat wearable", "Rare", "-", "-", "10", "-"],
+					["Eye Shape", "Rare", "8", "Flat Line", "92", "94 (13% possess)"],
+					["Eye Color", "Uncommon", "77", "Teal", "78", "33 (35% possess)"],
+                    ["Wizard Hat wearable", "Rare", "-", "-", "5", "-"],
                     ["Wizard Staff wearable", "Godlike", "-", "-", "50", "-"],
-                    ["TOTAL BRS", "-", "-", "-", "466", "-"],
+                    ["TOTAL BRS", "-", "-", "-", "539", "-"],
                     ["TOTAL ARS", "-", "-", "-", "-", "352"],
                     ["<b>FINAL RARITY SCORE</b>", "", "", "", "", "<b>818</b>"],
 
