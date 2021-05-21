ef32e8614ce3d69df9673f372e02db0491e40d2a
unintelligent-nerd <unintelligent.nerd@gmail.com>
2021-05-21 12:42:59 +0800

    Test uGOTCHI page

---
+++ data/tables/ugotchi.tsx
@@ -0,0 +1,69 @@
+
+
+interface Table {
+    tableName: string
+    tableData: TableData[]
+}
+
+interface TableData {
+    headers: any
+    data: any
+}
+
+export const tables
+    = [
+        {
+            tableName: "terenceMckenna",
+            tableCaption: "Gotchi Pocket",
+            tableData: {
+                headers: [],
+                data: [
+					[`<img src="/wearables/ethereum/GLDNXROSS ROBE.svg" alt = "aavegotchi GldnXross Robe" title = "GldnXross Robe" />`, `<img src="/wearables/link/sergey-beard.svg" alt = "aavegotchi chainlink sergey beard" title = "Sergey Beard" />`, `<img src="/wearables/ethereum/ALL SEEING EYES.svg" alt = "aavegotchi All Seeing Eyes" title = "All Seeing Eyes" />`, `<img src="/wearables/ethereum/GALAXY BRAIN.svg" alt = "aavegotchi Galaxy Brain" title = "Galaxy Brain" />`, `<img src="/wearables/ethereum/MUDGEN DIAMOND.svg" alt = "aavegotchi Mudgen Diamond" title = "Mudgen Diamond" />`, `<img src="/wearables/ethereum/MUDGEN DIAMOND.svg" alt = "aavegotchi Mudgen Diamond" title = "Mudgen Diamond" />`, `<img src="/wearables/layer2/Aavegotchi-Gen0-Sets-R4-Rofl-Mythical.svg" alt = "aavegotchi Mythical Rofl" title = "Mythical Rofl" />`]
+                ]
+            }
+        },
+		
+		{
+            tableName: "machineElf",
+            tableCaption: "Gotchi Pocket",
+            tableData: {
+                headers: [],
+                data: [
+					[`<img src="/wearables/layer2/Aavegotchi-Gen0-Sets-R4-Elf-Ears.svg" alt = "aavegotchi Elf Ears" title = "Elf Ears" />`, `<img src="/wearables/ethereum/CODERDAN SHADES.svg" alt = "aavegotchi Coderdan Shades" title = "Coderdan Shades" />`, `<img src="/wearables/ethereum/XIBOT MOHAWK.svg" alt = "aavegotchi Xibot Mohawk" title = "Xibot Mohawk" />`, `<img src="/wearables/final wearables/116_Coconut.svg" alt = "aavegotchi Coconut" title = "Coconut" />`, `<img src="/wearables/final wearables/89_MinerPickaxe.svg" alt = "aavegotchi Miner Pickaxe" title = "Pickaxe" />`]
+                ]
+            }
+        },
+		
+		{
+            tableName: "stonedApe",
+            tableCaption: "Gotchi Pocket",
+            tableData: {
+                headers: [],
+                data: [
+					[`<img src="/wearables/final wearables/91_PajamaPantsFull.svg" alt = "aavegotchi Pajama Shirt" title = "Pajama Shirt" />`, `<img src="/wearables/link/sergey-eyes.svg" alt = "aavegotchi chainlink sergey eyes" title = "Sergey Eyes" />`, `<img src="/wearables/ethereum/APE MASK.svg" alt = "aavegotchi Ape Mask" title = "Ape Mask" />`, `<img src="/wearables/layer2/Aavegotchi-Gen0-Sets-R4-Fireball.svg" alt = "aavegotchi Fireball" title = "Fireball" />`, `<img src="/wearables/final wearables/116_Coconut.svg" alt = "aavegotchi Coconut" title = "Coconut" />`, `<img src="/wearables/final wearables/110_JamaicanFlag.svg" alt = "aavegotchi Jamaican Flag" title = "Jamaican Flag" />`]
+                ]
+            }
+        },
+		
+		{
+            tableName: "ayahuasca",
+            tableCaption: "Gotchi Pocket",
+            tableData: {
+                headers: [],
+                data: [
+					[`<img src="/wearables/final wearables/85_GentlemanSuitFull.svg" alt = "aavegotchi Gentleman Coat" title = "Gentleman Coat" />`, `<img src="/wearables/aave/stani-hair.svg" alt = "aavegotchi aave stani hair" title = "Stani Hair" />`, `<img src="/wearables/ethereum/DAO EGG.svg" alt = "aavegotchi DAO Egg" title = "DAO Egg" />`, `<img src="/wearables/final wearables/96_WaterBottle.svg" alt = "aavegotchi Water Bottle" title = "Water bottle" />`, `<img src="/wearables/aave/captain-aave-suit.svg" alt = "aavegotchi aave captain aave suit" title = "Captain Aave Suit" />`, `<img src="/wearables/final wearables/66_FutureWizardVisor.svg" alt = "aavegotchi Wizard Visor" title = "Wizard Visor" />`, `<img src="/wearables/final wearables/104_PortalMageHelmet.svg" alt = "aavegotchi Portal Mage Helmet" title = "Portal Mage Helmet" />`, `<img src="/wearables/aave/captain-aave-shield.svg" alt = "aavegotchi aave captain aave shield" title = "Captain Aave Shield" />`]
+                ]
+            }
+        },
+		
+		{
+            tableName: "clockworkElf",
+            tableCaption: "Gotchi Pocket",
+            tableData: {
+                headers: [],
+                data: [
+					[`<img src="/wearables/layer2/Aavegotchi-Gen0-Sets-R4-Elf-Ears.svg" alt = "aavegotchi Elf Ears" title = "Elf Ears" />`, `<img src="/wearables/ethereum/CODERDAN SHADES.svg" alt = "aavegotchi Coderdan Shades" title = "Coderdan Shades" />`, `<img src="/wearables/layer2/Aavegotchi-Gen-0-Sets-Lil-Pump-Dreads.svg" alt = "aavegotchi Lil Pump Dreads" title = "Lil Pump Dreads" />`, `<img src="/wearables/ethereum/MUDGEN DIAMOND.svg" alt = "aavegotchi Mudgen Diamond" title = "Mudgen Diamond" />`, `<img src="/wearables/aave/aave-flag.svg" alt = "aavegotchi aave flag" title = "Aave Flag" />`]
+                ]
+            }
+        },
+    ]
